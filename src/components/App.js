fb96c102f5b7f43570776a188dd6972cec96447c
Aruna Turay <tura_arun@yahoo.com>
2023-06-14 10:06:41 -0400

    all good

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,32 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [message, setMessage] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((r) => r.json())
+      //.then(data => console.log(data.message))
+      .then((data) => {
+        setMessage(data.message)
+        setIsLoaded(true);
+      })
+    
+    
+  }, []);
+
+  if (!isLoaded) {
+     return <p>Loading...</p>;
+  }
+
+
+  return (
+    <div>
+      <img src={message} alt="A Random Dog" />
+    </div>
+  );
+}
+
+export default App;
